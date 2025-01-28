7a7712bc0d2e839a9df196df1e934ba0363ca2db
pushpalatha.E <preethirani777@gmail.com>
2025-01-28 19:09:22 +0530

    sumtwonum.java

---
+++ sumtwonum.java
@@ -0,0 +1,21 @@
+
+
+public class sumtwonum {
+    public static void main(String[] args)
+    {
+        int a = 15;
+        int b = 26;
+        int sum = a+b;
+        System.out.println(" The sum of " + a + " and " + b + " is: " +sum);
+    }  
+}
+
+
+OUTPUT:
+The sum of 15 and 26 is: 41
+
+
+DESCRIPTION:
+This the simple program to add two numbers by reading the user input and displays the output.
+In the above program the variable 'a' contains the value '15' and variable 'b' contains the value '26'.
+The addition of two numbers is performed and then stored in the name as sum by using the print statement the output of the program has been displayed.
