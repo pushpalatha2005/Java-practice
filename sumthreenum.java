e7f81f6e73a2263157e5b46d7f8873977ae1b2dc
pushpalatha.E <preethirani777@gmail.com>
2025-01-28 19:11:46 +0530

    sumthreenum.java

---
+++ sumthreenum.java
@@ -0,0 +1,18 @@
+public class sumthreenum {
+    
+    public static void main(String[] args)
+    {
+        int a = 13;
+        int b = 56;
+        int c = 17;
+        int sum = a+b+c;
+        System.out.println(" The sum of numbers " + a + " , " + b +" and " + c + " is: " + sum);
+    }
+}
+
+
+OUTPUT:
+The sum of numbers 13 , 56 and 17 is: 86
+
+
+DESCRIPTION:
