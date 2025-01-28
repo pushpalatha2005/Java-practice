ff7e07ac69b4fa6a1b79bdfd901e58902f4ad418
pushpalatha.E <preethirani777@gmail.com>
2025-01-28 19:13:49 +0530

    sumthreenum.java

--- sumthreenum.java
+++ sumthreenum.java
@@ -16,3 +16,6 @@ The sum of numbers 13 , 56 and 17 is: 86
 
 
 DESCRIPTION:
+This the simple program to add three numbers by reading the user input and displays the output.
+In the above program the variable 'a' contains the value '13' , variable 'b' contains the value '56' and then variable 'c' contains the value '17' .
+The addition of three numbers is performed and then stored in the name as sum by using the print statement "System.out.println()" the output of the program has been displayed.
