a49e2f468a61bfc43c434c2a0042d68371daaec7
pushpalatha.E <preethirani777@gmail.com>
2025-01-28 18:56:21 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+    
+}
+
+
+
+
+OUTPUT:
+Hello World!
+
+DESCRIPTION:
+This is a simple program to print the word "Hello World!" by using the System.out.println() to print the word.
